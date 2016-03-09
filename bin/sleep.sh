a64070dd1475fe6cec1684e9571e23729c310ccc
Juan C. Müller <jcmuller@gmail.com>
2016-03-09 09:55:33 -0500

    Different way to sleep

--- bin/sleep.sh
+++ bin/sleep.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-i3lock -c 000000 -e -b -d -I 2 && echo mem | sudo tee /sys/power/state
+#i3lock -c 000000 -e -b -d -I 2 && echo mem | sudo tee /sys/power/state
+#i3lock -c 000000 -e -b -d -I 2 && dbus-send --system --print-reply  --dest="org.freedesktop.UPower" /org/freedesktop/UPower org.freedesktop.UPower.Suspend
+i3lock -c 000000 -e -b -d -I 2 && sudo /usr/sbin/pm-suspend
