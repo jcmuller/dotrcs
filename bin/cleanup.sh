7db8552a6be36e8e9982b0b9320cc8a32e5b3cb1
Juan C. Müller <jcmuller@gmail.com>
2019-03-31 15:04:28 -0400

    Add more stuff

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,1 +1,1 @@
-find  . -type f \( -name '*~' -or -name '.*.sw?' \) -print -exec rm {} \;
+find  . -type f \( -name '*~' -or -name '.*.sw?' -or -name '*.orig' -or -name '#*#' -or -name '.#*' \) -print -exec rm {} \;
